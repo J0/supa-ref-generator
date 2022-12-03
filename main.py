a86d9ffff4d1ee22752f6649a9f4068679a5fe4e
joel@joellee.org <joel@joellee.org>
2022-12-03 21:27:07 +0200

    fix: add initial generation

--- main.py
+++ main.py
@@ -11,26 +11,30 @@ def fetch_docstrings():
     raw_tree = py_file.read_text()
     tree = ast.parse(raw_tree)
     functions = [f for f in ast.walk(tree) if isinstance(f, ast.FunctionDef)]
+    py_func_names = [f.name for f in functions]
     # For each function
     js_function_reference = fetch_js_function_list(JS_FILE_PATH)
     function_docs = [ast.get_docstring(f) for f in functions]
-    res = []
-    # for function in functions:
-    #     if snake_to_camel_case(function) in js_function_reference:
-    #         yml_entry = create_yml_entry(function)
-    #         res.append(yml_entry)
+    # fetch function names
+    js_func_names = [x.get('title').split(".")[-1].strip("()") for x in js_function_reference if x.get('title') is not None]
+    output = {'pages':{}}
+    for function in functions:
+        pyfunc_as_camel = snake_to_camel_case(function.name)
+        if pyfunc_as_camel in js_func_names:
+            print(f"{pyfunc_as_camel}")
+            output = add_yml_entry(output, function.name, pyfunc_as_camel, js_function_reference)
 
-    # Write res to yml file
-    import pdb;pdb.set_trace()
     with open('./py_spec_v2.yml', 'w') as pyspec:
-        yaml.dump(spec_dict, pyspec)
+        yaml.dump(output, pyspec)
 
-    return function_docs
+    # return function_docs
 
-def create_yml_entry():
-    # If it has a ref, we grad the title, examples and name.
-    # To determine the name we split by "." and take the last entry
-    pass
+def add_yml_entry(output, function_py_name,js_func_name, js_function_reference):
+    # Top Level entry is pages
+    matching_title = list(filter(lambda ref: ref.get('title_without_prefix') == js_func_name, js_function_reference))
+    if len(matching_title) > 0:
+        output['pages'][function_py_name] = matching_title
+    return output
 
 def fetch_js_function_list(spec_path: str):
     func_list = []
@@ -44,6 +48,8 @@ def fetch_js_function_list(spec_path: str):
             for index,entry in enumerate(top_level):
                 func_entry = {}
                 func_entry['title'] = top_level_title[index]
+                # Remove the "auth." in  "auth.signUp". We also remove ()
+                func_entry['title_without_prefix'] = top_level_title[index].split(".")[-1].strip("()")
                 if entry.get('$ref'):
                     for field in fields_to_copy:
 
@@ -56,10 +62,9 @@ def fetch_js_function_list(spec_path: str):
 def is_valid_function_name(name):
     pass
 
-def snake_to_camel_case(name):
-    pattern = re.compile(r'(?<!^)(?=[A-Z])')
-    name = pattern.sub('_', name).lower()
-    return name
+def snake_to_camel_case(snake_str):
+    first, *others = snake_str.split('_')
+    return ''.join([first.lower(), *map(str.title, others)])
 
 if __name__ == "__main__":
     s = fetch_docstrings()
